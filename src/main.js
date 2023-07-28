6b60694507bd4b9ea78257a54a5a6fd8b1497fb1
shayandrg <shayan.derogarian@gmail.com>
2023-07-28 13:00:18 +0330

    first things

--- src/main.js
+++ src/main.js
@@ -4,17 +4,20 @@ const fs = require('fs');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+const rgbColorPalettes = {
+	'Sunset': [ '#30ff90', '#ed2ded', '#c99826', '#30ffe6' ],
+	'Instagram': [ '#7638fa', '#ffd600', '#ff7a00', '#ff0169', '#d300c5', '#7638fa' ]
+}
+var config = {};
+
+const reloadConfig = () => {
+	config = vscode.workspace.getConfiguration("rgbCode")
+}
+reloadConfig()
+
 function activate(context) {
-	vscode.window.showInformationMessage('kosssssssssssssssss');
 
-	let disposable = vscode.commands.registerCommand('test2.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-		console.log('Congratulations, your extension "test2" is now active!');
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test2!');
-	});
-
-	let active = vscode.commands.registerCommand('test2.active', function () {
+	let active = vscode.commands.registerCommand('RGBCode.active', function () {
 		const isWin = /^win/.test(process.platform);
 		const appDir = path.dirname(require.main.filename);
 		const base = appDir + (isWin ? "\\vs\\code" : "/vs/code");
@@ -24,26 +27,31 @@ function activate(context) {
 		try {
 			const rawHtml = fs.readFileSync(htmlFile, "utf-8");
 			const isRGBCodeExist = rawHtml.includes("RGBCode-style-element");
-			const css = fs.readFileSync(__dirname +'/main.css', 'utf-8');
+			let css = fs.readFileSync(__dirname +'/main.css', 'utf-8');
 
-			if (!isRGBCodeExist) {
-				const vscodeDOM = new JSDOM(rawHtml);
-				let head = vscodeDOM.window.document.head || vscodeDOM.window.document.getElementsByTagName('head')[0];
-				let style = vscodeDOM.window.document.createElement('style');
-				head.appendChild(style);
-				style.type = 'text/css';
-				style.setAttribute("id", "RGBCode-style-element");
-				style.appendChild(vscodeDOM.window.document.createTextNode(css));
-		
-				fs.writeFileSync(htmlFile, vscodeDOM.serialize(), "utf-8");
-				vscode.commands.executeCommand("workbench.action.reloadWindow");
+			let activePalette = rgbColorPalettes[config.get('palette')] || rgbColorPalettes.Sunset;
+			css = css.replace(/\[TAB_COLORS\]/g, activePalette.join(', '));
+
+			const vscodeDOM = new JSDOM(rawHtml);
+			if (isRGBCodeExist) {
+				vscodeDOM.window.document.querySelectorAll('#RGBCode-style-element').forEach(el => el.remove())
 			}
+
+			let head = vscodeDOM.window.document.head || vscodeDOM.window.document.getElementsByTagName('head')[0];
+			let style = vscodeDOM.window.document.createElement('style');
+			head.appendChild(style);
+			style.type = 'text/css';
+			style.setAttribute("id", "RGBCode-style-element");
+			style.appendChild(vscodeDOM.window.document.createTextNode(css));
+	
+			fs.writeFileSync(htmlFile, vscodeDOM.serialize(), "utf-8");
+			vscode.commands.executeCommand("workbench.action.reloadWindow");
 		} catch(e) {
 			console.log('kir shodi', e);
 		}
 	});
 
-	let deActive = vscode.commands.registerCommand('test2.deActive', function () {
+	let deActive = vscode.commands.registerCommand('RGBCode.deActive', function () {
 
 		const isWin = /^win/.test(process.platform);
 		const appDir = path.dirname(require.main.filename);
@@ -67,9 +75,16 @@ function activate(context) {
 		}
 	});
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(active);
 	context.subscriptions.push(deActive);
+
+	vscode.workspace.onDidChangeConfiguration(event => {
+        let affected = event.affectsConfiguration("rgbCode.palette");
+        if (affected) {
+			reloadConfig()
+            vscode.commands.executeCommand("RGBCode.active");
+        }
+    })
 }
 
 // This method is called when your extension is deactivated
